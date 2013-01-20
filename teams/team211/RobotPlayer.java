1dbd00ba10611189d5070785a83a76c300719cb6
itsnotmyfault1 <itsnotmyfault1@gmail.com>
2013-01-19 21:08:53 -0500

    Trying to fix cappers. going to atlantic city instead.

--- teams/team211/RobotPlayer.java
+++ teams/team211/RobotPlayer.java
@@ -289,6 +289,8 @@ public class RobotPlayer {
 						if (my_loc.equals(camp_goal)) {
 							if (build_encampment())
 								camp_goal = null;
+						} else if (rc.senseObjectAtLocation(camp_goal).getTeam() == my_team) {
+							r_soilder_assault(); /* The cappers spin in cicles when there's a guy already on the spot trying to cap it. This attempted to fix it and failed. Now no cappers spawn.*/
 						} else {	
 							goToLocation(camp_goal);
 						}
